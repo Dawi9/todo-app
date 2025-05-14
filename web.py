b2def58d8fbc2f8f60b32f25afca6154f1364ae8
Dawi908 <83314945+Dawi908@users.noreply.github.com>
2025-05-13 20:13:03 -0700

    Added the todo add button on the website

--- web.py
+++ web.py
@@ -3,14 +3,21 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    new_todo = st.session_state['new_todo']
+    todos.append(new_todo + "\n")
+    functions.write_todos(todos)
+    st.session_state['new_todo'] = ''
+
+
 st.title("My Todo App")
 st.subheader("This is my first todo app.")
 st.write("This app will help you manage your tasks.")
 
-
-
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label= "", placeholder= "Add new todo...")
-st.button("Add todo)
+st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
+st.button("Add todo", on_click=add_todo)
+
