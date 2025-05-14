ed7afaf11be4ca0298dbb7c0c49df724e944cc5e
Dawi908 <83314945+Dawi908@users.noreply.github.com>
2025-05-13 20:47:29 -0700

    add the completed Check so you can delete the task by just clicking on it.

--- web.py
+++ web.py
@@ -5,21 +5,19 @@ todos = functions.get_todos()
 
 
 def add_todo():
-    todo = st.session_state['new_todo']
-    todos.append(todo + "\n")
-    functions.write_todos(todos)
-    st.session_state['new_todo'] = ''
+    todo = st.session_state['new_todo'].strip()
+    if todo:
+        todos.append(todo + "\n")
+        functions.write_todos(todos)
+        st.session_state['new_todo'] = ''
 
 
-
-
-
-st.title("My Todo App")
+st.markdown("<h1 style='color:  blue;'>My Todo App</h1>", unsafe_allow_html=True)
 st.subheader("This is my first todo app.")
 st.write("This app will help you manage your tasks.")
 
-for index, todo in enumerate (todos):
-    checkbox = st.checkbox(todo, key =todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo.strip(), key=todo)
     if checkbox:
         todos.pop(index)
         functions.write_todos(todos)
@@ -27,5 +25,4 @@ for index, todo in enumerate (todos):
         st.rerun()
 
 st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
-st.button("Add todo", on_click=add_todo)
-
+st.button("Add", on_click=add_todo)
